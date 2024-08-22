4059d252145b59b39288e6a3dc713b6a0631c5ee
Ewerton Beck <ewertonbeck@hotmail.com>
2024-08-22 07:57:11 -0300

    Adicionada a pasta de Exercicios

---
+++ Exercícios Python/ex001.py
@@ -0,0 +1,4 @@
+#CRIE UM PROGRAMA QUE IMPRIMA 'OLÁ MUNDO'
+
+msg= 'Olá mundo'
+print(msg)
---
+++ Exercícios Python/ex002.py
@@ -0,0 +1,12 @@
+#FAÇA UM PROGRAMA QUE LEIA O NOME DE UMA PEASSOA E MOSTE UMA MENSAGEM DE BOAS VINDAS.
+
+nome0= input('Dgigite o seu nome ')
+print('Olá, seja bem vindo ',nome0)
+
+nome1= input('Qual é o seu nome ?')
+print('Olá, seja bem vindo, {}!' .format (nome1))
+
+nome2= input('Me fale o seu nome.' )
+print(f"O seu nome é {nome2}, como vai?")
+
+#existem tres modos de resolver esse exercício
